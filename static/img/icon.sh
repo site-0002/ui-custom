8789caa0d7c82c554571196505af69198bdbc7ce
Kitsune Solar <kitsune.solar@gmail.com>
2021-05-18 17:36:54 +0300

    2021-05-18 14:36:54

--- static/img/icon.sh
+++ static/img/icon.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/bash
 
-png() {
-  size=(16 24 32 48 64 72 80 96 128 144 152 167 180 192 196 256 300 512)
+size=(16 24 32 48 64 72 80 96 128 144 152 167 180 192 196 256 300 512)
+file_svg="icon.svg"
+file_ico="icon.ico"
 
+png() {
+  check_file
   for i in "${size[@]}"; do
-    inkscape -w "${i}" -h "${i}" -b ffffff -o icon-"${i}".png icon.svg
+    inkscape -w "${i}" -h "${i}" -b ffffff -o icon-"${i}".png "${file_svg}"
   done
 }
 
 ico() {
-  convert -density 256x256 -background "#ffffff" icon.svg -define icon:auto-resize -colors 256 icon.ico
-  identify icon.ico
+  check_file
+  convert -density 256x256 -background "#ffffff" "${file_svg}" -define icon:auto-resize -colors 256 "${file_ico}"
+  identify "${file_ico}"
+}
+
+check_file() {
+  if [[ ! -f "${file_svg}" ]]; then
+    printf '%s does not exist!\n' "${file_svg}"
+    exit 1
+  fi
 }
 
 "$@"
